314094d05bd5ab6e5395ab1eae60a93117e9bcea
shivsprk <shivsprk@gmail.com>
2022-02-02 17:14:51 +0530

    excel project

---
+++ src/org/excel/ExcelData.java
@@ -0,0 +1,15 @@
+package org.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ExcelData {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("C:\\Users\\sivapriyan\\Eclipseoxygen\\Excel\\Excel\\exceltask.xlsx");
+		FileInputStream stream = new FileInputStream(file);
+		
+		
+	}
+
+}
